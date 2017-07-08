8c24aea1855a5a02bffd7f0b11b29325b6de373d
Kent Henneuse <khenneuse@marchex.com>
2017-07-07 19:08:10 -0700

    Moved to class instead of functional component

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,20 @@
 import * as React from "react";
+import { Component } from "react";
 import * as ReactDOM from "react-dom";
 
 import { Hello } from "./components/Hello";
 
+const API_KEY = 'AIzaSyCbIO-Q9YAilLrCym_0pA4poa4M4uBW4T8';
+
+class App extends Component<{}, {}> {
+  render() {
+    return (
+      <Hello compiler="TypeScript" framework="React" />
+    );
+  }
+}
+
 ReactDOM.render(
-  <Hello compiler="TypeScript" framework="React" />,
+  <App />,
   document.getElementById("app")
 );
